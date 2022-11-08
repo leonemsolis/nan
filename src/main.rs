7a46302e6046da0330f06f5b69761dd46bfa8c5e
Ruslan <ruslan@MacBook-Pro.local>
2022-11-08 22:59:59 +0600

    Setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use nannou::prelude::*;
 use nannou::image::{RgbImage, DynamicImage, Rgb};
 
-const SCREEN_WIDTH: u32 = 400;
-const SCREEN_HEIGHT: u32 = 400;
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 400;
 
 fn main() {
     nannou::app(model)
@@ -16,7 +16,7 @@ struct Model {
 fn model(app: &App) -> Model {
     app.new_window()
         .title(app.exe_name().unwrap())
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .size(WIDTH, HEIGHT)
         .view(view)
         .build()
         .unwrap();
@@ -28,12 +28,17 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
-    let mut img = RgbImage::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let mut img = RgbImage::new(WIDTH, HEIGHT);
 
-    for x in 15..=17 {
-        for y in 8..24 {
-            img.put_pixel(x, y, Rgb([255, 0, 0]));
-            img.put_pixel(y, x, Rgb([255, 0, 0]));
+    let center = Vec2::new((WIDTH / 2) as f32, (HEIGHT / 2) as f32);
+    for x in 0..WIDTH {
+        for y in 0..HEIGHT {
+            let point = Vec2::new(x as f32, y as f32);
+            let d = center.distance(point);
+            let d = clamp(d, 0.0, 255.0) as u8;
+
+            let color = Rgb([d, d, d]);
+            img.put_pixel(x, y, color);
         }
     }
 
